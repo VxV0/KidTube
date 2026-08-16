b714bed5b54d22de622d93e5e3b7e925338c5f9f
VxV0 <muomiga925@gmail.com>
2026-08-16 18:05:33 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:provider/provider.dart';
 import 'providers/app_provider.dart';
 import 'screens/main_shell.dart';
 import 'utils/constants.dart';
+import 'package:media_kit/media_kit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  MediaKit.ensureInitialized(); // ADD THIS
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.light,
